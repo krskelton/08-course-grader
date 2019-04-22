b60072db403462932324dc709174ae91ef8b7570
krskelton <kskelton82@gmail.com>
2019-04-22 15:27:05 -0600

    Update course_grader.py

--- course_grader.py
+++ course_grader.py
@@ -1,2 +1,15 @@
-def course_grader(param):
-    return ""
+import math
+
+def course_grader(test_scores):
+    # Your code here
+    length_of_list = len(test_scores)    
+    average_score = sum(test_scores) / length_of_list
+    pass_fail = ""
+    
+    for test_score in test_scores:               
+        if average_score >= 70 and test_score > 50:
+            pass_fail = "pass"            
+        elif average_score < 70 or test_score < 50:
+            pass_fail = "fail"
+            return pass_fail         
+    return pass_fail
